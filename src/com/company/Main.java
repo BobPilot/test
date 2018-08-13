74db270fea7702af28e3c43474547227c66883b1
BobPilot <bobpilott@gmail.com>
2018-08-13 09:46:32 +0300

    First chenge of project

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,11 +5,16 @@ public class Main {
     public static void main(String[] args) {
 
         int i = 0;
-        while (i < 10){
-            if(i == 5){
-                throw new RuntimeException("AAA");
+        try {
+            while (i < 10) {
+                if (i == 5) {
+                    throw new RuntimeException("AAA");
+                }
+                i++;
+
             }
-            i++;
+        } catch (RuntimeException e) {
+            System.out.println(e.getMessage());
         }
 
 
